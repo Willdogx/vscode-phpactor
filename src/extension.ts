580c855a5515cbb2e9d1a551713417aa54ed173b
Santiago Alvarez <santiago.alvarez@check24.es>
2021-03-27 20:44:21 +0100

    add command to create new php class

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,15 @@
+import { spawn } from 'child_process';
 import {
     LanguageClient,
     ServerOptions,
     LanguageClientOptions,
+    RevealOutputChannelOn,
 } from "vscode-languageclient";
 
 import * as vscode from "vscode";
+import { promisify } from "util";
 
+const exec = promisify(require('child_process').exec);
 const LanguageID = 'php';
 
 let languageClient: LanguageClient;
@@ -50,7 +54,8 @@ function createClient(config: any): LanguageClient {
             { language: LanguageID, scheme: 'file' },
             { language: LanguageID, scheme: 'untitled' }
         ],
-        initializationOptions: config.config
+        revealOutputChannelOn: RevealOutputChannelOn.Never,
+        initializationOptions: config.config,
     };
 
     languageClient = new LanguageClient(
@@ -64,6 +69,7 @@ function createClient(config: any): LanguageClient {
     vscode.commands.registerCommand('phpactor.config.dump', dumpConfig);
     vscode.commands.registerCommand('phpactor.services.list', servicesList);
     vscode.commands.registerCommand('phpactor.status', status);
+    vscode.commands.registerCommand('phpactor.class_new', newClass);
 
     return languageClient;
 }
@@ -98,4 +104,75 @@ function status(): void {
     }
 
     languageClient.sendRequest('system/status');
+}
+
+async function newClass(file: any): Promise<void> {
+    if (!languageClient) {
+        return;
+    }
+
+    const workingDir = vscode.workspace.getWorkspaceFolder(vscode.window.activeTextEditor.document.uri).uri.path;
+    let currentPath: string;
+    if (file instanceof vscode.Uri) {
+        currentPath = file.path;
+    } else {
+        currentPath = vscode.window.activeTextEditor.document.fileName;
+    }
+
+    
+    const newClassPath = await vscode.window.showInputBox({
+        placeHolder: 'Path to new file',
+        prompt: 'Path to new file',
+        value: currentPath
+    });
+
+    if (newClassPath === undefined) {
+        return;
+    }
+
+    const { error, stdout, stderr } = await exec(`phpactor class:new ${newClassPath}`, { cwd: workingDir });
+    
+    if (error) {
+        languageClient.outputChannel.appendLine(error.message);
+        return;
+    }
+    
+    if (stderr) {
+        languageClient.outputChannel.appendLine(stderr);
+        return;
+    }
+
+    languageClient.outputChannel.appendLine(stdout);
+
+    const textDocument = await vscode.workspace.openTextDocument(vscode.Uri.parse(newClassPath));
+    vscode.window.showTextDocument(textDocument);
+}
+
+function rpcPhpActor(workingDir: string, command: { action: string, parameters: any}): Promise<any> {
+    if (command.action === 'return') {
+      return command.parameters.value;
+    }
+    return new Promise((resolve, reject) => {
+      const sh = 'phpactor rpc';
+      const cmd = spawn(sh, {
+        cwd: workingDir,
+        shell: '/bin/bash'
+      });
+
+      cmd.stdin.write(JSON.stringify(command));
+
+      let result = '';
+      cmd.stdout.on('data', function (data: Buffer) {
+        result += data.toString();
+      });
+
+      cmd.on('close', function () {
+        resolve(JSON.parse(result));
+      });
+
+      cmd.on('error', function (err: Error) {
+        reject(err);
+      });
+      cmd.stdin.end();
+    });
 }
